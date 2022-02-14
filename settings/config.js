7a0673028fdab0343ccb1018b9760ea5d862a80f
RajaBOTz1 <99687830+RajaBOTz1@users.noreply.github.com>
2022-02-15 04:06:08 +0800

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -5,9 +5,9 @@ const chalk = require('chalk')
 global.self = false //jadiin true klo gk mau fitur bot lu di pke sama org lain
 
 // setting
-global.ownername ="𝐟𝐢𝐫𝐚𝐚"
-global.ownernumber = "13022814650"
-global.botname = "𝐟𝐢𝐫𝐚𝐚ʙᴏᴛ"
+global.ownername ="RAJA`"
+global.ownernumber = "6282188585604"
+global.botname = "Raja BOTz"
 global.thumbnail = fs.readFileSync("./settings/haruka.jpg") //sesuaikan dengan nama foto
 global.background = "https://telegra.ph/file/d4c05638fa7886a1d8060.jpg"
 global.limit = {
